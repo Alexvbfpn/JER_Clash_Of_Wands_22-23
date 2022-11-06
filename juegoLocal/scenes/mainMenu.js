376cd7fbd5f055845b714bca84597054d1fa8c63
Alexvbfpn <campbell12082000@gmail.com>
2022-11-06 19:09:27 +0100

    Botón en menu principal para pasar a siguiente pantalla

--- juegoLocal/scenes/mainMenu.js
+++ juegoLocal/scenes/mainMenu.js
@@ -7,11 +7,28 @@ export class MainMenu extends Phaser.Scene
 
     preload()
     {
-        this.load.image('mainMenu_Background', '../assets/img_mainMenu.jpg');
+        this.load.image('mainMenu_Background', 'assets/img_mainMenu.jpg');
     }
 
     create()
     {
         this.add.image(960, 540, 'mainMenu_Background');
+        var play_button = this.add.image(0, 0, 'assets/play_button.png');
+        var text_play_button = this.add.image(0, 0, 'play_button_text');
+
+        var container = this.add.container(400, 300, [play_button, text_play_button]);
+
+        container.setSize(play_button.width, play_button.height);
+        container.setInteractive();
+
+        container.on('pointover', function (){
+
+            play_button.setTint(0x44ff44);
+        });
+
+        container.on('pointerout', function (){
+
+            play_button.clearTint();
+        });
     }
 }
