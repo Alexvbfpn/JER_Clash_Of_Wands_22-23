7b23e4dee1718eb121e030c93eb8fb7222d61630
Alexvbfpn <campbell12082000@gmail.com>
2022-11-30 01:18:12 +0100

    Fallos

--- juegoLocal/master/game_Local.js
+++ juegoLocal/master/game_Local.js
@@ -1,21 +1,16 @@
 import {MainMenu} from './scenes/mainMenu.js';
 import {Match} from './scenes/match.js';
 import {CharacterSelector} from './scenes/characterSelector.js';
+
 import {MidScene} from "./scenes/midScene.js";
 import {FinalScene} from "./scenes/finalScene.js";
 import {Credits} from "./scenes/credits.js";
 import {Controls} from "./scenes/controls.js";
 
-
 var scenes = [];
 scenes.push(MainMenu);
 scenes.push(CharacterSelector);
 scenes.push(Match);
-
-
-
-
-
 
 var config = {
 
