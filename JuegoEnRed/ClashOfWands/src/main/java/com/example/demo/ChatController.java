94d18646aedb8628723149498ed2c5c03ac95247
VictorCabanillas <115984937+VictorCabanillas@users.noreply.github.com>
2022-12-21 03:34:11 +0100

    Add files via upload

---
+++ JuegoEnRed/ClashOfWands/src/main/java/com/example/demo/ChatController.java
@@ -0,0 +1,70 @@
+package com.example.demo;
+import org.springframework.web.bind.annotation.*;
+import java.io.*;
+import java.util.Stack;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+@RestController
+public class ChatController {
+	
+	Stack<String> messages = new Stack<String>();
+	String chatFileURL = "src/main/resources/static/db/chat.txt"; 
+	
+	@GetMapping
+	public Stack<String> messages() //@PathVariable("i") String i
+	{
+		return messages;
+	}
+	
+	@PostMapping("/chat")
+	public void postMessage(@RequestBody Message message) throws FileNotFoundException {
+
+		String contents = "";
+        contents = message.getUser() + ": " + message.getMessage() + System.getProperty("line.separator");
+        try (Writer writer = new BufferedWriter(new FileWriter(chatFileURL, true))) 
+        {
+            writer.write(contents);
+            writer.close();
+            System.out.println("User written succesfully");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error writing user");
+        }
+        messages.add(contents);
+	}
+	@GetMapping("/chat/{i}")
+	public String getMessage(@PathVariable("i") String i) {
+		
+		int size = messages.size();
+		
+		String message = "";
+		if(i.equals("0") && size>=1) {
+			message = messages.get(size-1);
+		}
+		if(i.equals("1") && size>=2) {
+			message = messages.get(size-2);
+		}
+		if(i.equals("2") && size>=3) {
+			message = messages.get(size-3);
+		}
+		if(i.equals("3") && size>=4) {
+			message = messages.get(size-4);
+		}
+		if(i.equals("4") && size>=5) {
+			message = messages.get(size-5);
+		}
+		if(i.equals("5") && size>=6) {
+			message = messages.get(size-6);
+		}
+		if(i.equals("6") && size>=7) {
+			message = messages.get(size-7);
+		}
+		if(i.equals("7") && size>=8) {
+			message = messages.get(size-8);
+		}
+			
+		return message; 
+	}
+}
